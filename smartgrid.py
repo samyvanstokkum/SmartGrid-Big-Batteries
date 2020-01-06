c23f177456d2f2312317aaa635c8c92e33000248
Amparo Gilhuis <amparo.gilhuis@student.uva.nl>
2020-01-06 14:01:04 +0100

    new smartgrid

--- smartgrid.py
+++ smartgrid.py
@@ -1,6 +1,21 @@
-from loading import district, battery
+import csv
+import House from house
 
 # retrieve district
+def get_info_district1():
+
+    f = open('wijk1_huizen.csv')
+
+    district1_data = csv.reader(f)
+    next(district1_data)
+    district1 = []
+
+    for row in district1_data:
+        district1.append(House(int(row[0]), int(row[1], float(row[2]))))
+
+    # comment blabla
+    print(district1)
+
 district()
 
 # retrieve batteries
