fc4a806c513b6310f2fba358bf39930ff2888e43
Amparo Gilhuis <amparo.gilhuis@student.uva.nl>
2020-01-07 11:13:00 +0100

    added plot

--- smartgrid.py
+++ smartgrid.py
@@ -1,12 +1,38 @@
 import csv
 from house import House
 from battery import Battery
+import matplotlib.pyplot as plt
 
 
 def main():
+    # set to 1 if you want to print the houses and batteries
+    print = 1
+
     # district_number = input("What district?:")
-    district = create_district(1)
-    batteries = create_batteries(1)
+    district, x_batteries, y_batteries = create_district(1)
+    batteries, x_houses, y_houses = create_batteries(1)
+
+    # print batteries and houses
+    if print == 1:
+        plt.figure()
+
+        # plot batteries in red and houses in blue
+        plt.plot(x_batteries, y_batteries, 'ro', label='batteries')
+        plt.plot(x_houses, y_houses, 'bo', label='houses')
+
+        # plt.title('houses and batteries')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+    # just to check if can add/remove houses
+    batteries[1].add_house(district[1])
+    print(batteries[1].houses)
+    print(batteries[1].capacity)
+
+    batteries[1].remove_house(district[1])
+    print(batteries[1].houses)
+    print(batteries[1].capacity)
 
 
 def create_district(district_number):
@@ -15,13 +41,19 @@ def create_district(district_number):
     district_data = csv.reader(f)
     next(district_data)
     district = {}
+    x_houses = []
+    y_houses = []
 
     # Create an object for each house
     for house_id, row in enumerate(district_data, 1):
         district[house_id] = House(house_id, int(
             row[0]), int(row[1]), float(row[2]))
 
-    return district
+        # append coordinates for later use (plotting the batteries)
+        x_houses.append(int(row[0]))
+        y_houses.append(int(row[1]))
+
+    return district, x_houses, y_houses
 
 
 def create_batteries(district_number):
@@ -30,6 +62,8 @@ def create_batteries(district_number):
     batteries_data = csv.reader(f)
     next(batteries_data)
     batteries = {}
+    x_batteries = []
+    y_batteries = []
 
     # Create an object for each house
     for battery_id, row in enumerate(batteries_data, 1):
@@ -37,9 +71,13 @@ def create_batteries(district_number):
         batteries[battery_id] = Battery(
             battery_id, coordinates[0], coordinates[1], eval(row[1]))
 
+        # append coordinates for later use (plotting the batteries)
+        x_batteries.append(coordinates[0])
+        y_batteries.append(coordinates[1])
+
     print(batteries[1])
 
-    return batteries
+    return batteries, x_batteries, y_batteries
 
 
 if __name__ == "__main__":
