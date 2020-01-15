fb3ba056ab45c41a9d3f03cd0cb75014455be9e4
amparogilhuis <amparo.gilhuis@student.uva.nl>
2020-01-15 13:19:55 +0100

    new structured files + random

--- grid.py
+++ grid.py
@@ -1,37 +1,172 @@
 # Libraries
 import matplotlib.pyplot as plt
+import random
+import numpy as np
 
-
-def create_grid(district, x_houses, y_houses, batteries, routes):
-
-    # set to True to print the houses and batteries
-    yes_plot = True
-
-    # print batteries and houses
-    if yes_plot:
-        plt.figure()
-        colors = ['r', 'b', 'g', 'c', 'y']
-        # plot batteries in red and houses in blue
-        for battery in batteries:
-            plt.plot(battery.x, battery.y,
-                     colors[battery.id-1]+'o', markersize=10, label='batteries')
-
-        plt.plot(x_houses, y_houses, 'k*', label='houses')
-
-        # plt.title('houses and batteries')
-        plt.grid(True)
-        plt.legend(loc='upper center', ncol=10, fontsize=8)
-
-        # append house to battery
+def swap(routes, batteries, iteration, random_swap_every_x, optimization_type):
     
-        cable_distance = 0
+    # choose random battery and random house
+    random_battery_nr = random.randint(1,5)
+    random_battery = batteries[random_battery_nr -1]
+    random_house = random.choice(random_battery.houses)
 
+    # get index of battery number and delete the current battery 
+    all_battery_nr = [1,2,3,4,5]
+    index_rand_batt = all_battery_nr.index(random_battery_nr)
+    del all_battery_nr[index_rand_batt] 
+
+    # empty list for all swap_options
+    swap_options = []
+
+    # loop through all batteries and their houses to check if a swap is possible and what the cost would be
+    for current_battery_nr in all_battery_nr:       
+        current_battery = batteries[current_battery_nr-1]
+        
+        for house in current_battery.houses:
+            random_battery_cap = random_battery.capacity + random_house.usage
+
+            # check if swap is possible capacity-wise
+            if house.usage < random_battery_cap and random_house.usage < current_battery.capacity + house.usage:
+                
+                # calculate current and new distance
+                current_distance = abs(house.x - current_battery.x) + abs(house.y - current_battery.y) + abs(random_house.x - random_battery.x) + abs(random_house.y - random_battery.y)
+                new_distance = abs(random_house.x - current_battery.x) + abs(random_house.y - current_battery.y) + abs(house.x - random_battery.x) + abs(house.y - random_battery.y)
+                
+                # distance_difference is positive when new distance is closer
+                distance_difference = current_distance - new_distance
+
+                # save every option as tuple in a list
+                swap_options.append((current_battery, house, distance_difference))
+   
+    go_on = False
+    
+    # check if want to make swap happen (only when swap_options are present) and:
+    if swap_options:
+
+        # when it's time for random swap
+        if iteration % random_swap_every_x == 0:
+            go_on = True
+
+        # when stochastic HC, so always random swap
+        elif optimization_type == "stochastic_hill_climber":
+            go_on = True
+        
+        # when a swap option leads to less costs
+        elif max(swap_options, key= lambda x: x[2])[2] > 0:
+            go_on = True
+
+    # make swap
+    if go_on == True:
+        
+        # choose battery and house with minimal costs
+        if iteration % random_swap_every_x != 0:
+            desired_battery = max(swap_options, key= lambda x: x[2])[0]
+            house_to_extract = max(swap_options, key= lambda x: x[2])[1]
+
+        elif optimization_type == "stochastic_hill_climber":
+            random_battery_nr = random.choice(all_battery_nr)
+            desired_battery = batteries[random_battery_nr -1]
+            house_to_extract = random.choice(desired_battery.houses)
+
+        # choose random battery and house
+        else:
+            random_battery_nr = random.choice(all_battery_nr)
+            desired_battery = batteries[random_battery_nr -1]
+            house_to_extract = random.choice(desired_battery.houses)
+
+        # remove house from desired battery
+        desired_battery.remove_house(house_to_extract)
+        x = [house_to_extract.x, house_to_extract.x, desired_battery.x]
+        y = [house_to_extract.y, desired_battery.y, desired_battery.y]
+        routes[desired_battery].remove((x, y))
+
+        # remove random house from random battery
+        random_battery.remove_house(random_house)
+        x = [random_house.x, random_house.x, random_battery.x]
+        y = [random_house.y, random_battery.y, random_battery.y]
+        routes[random_battery].remove((x, y))
+
+        # add house to extract to random battery
+        x = [house_to_extract.x, house_to_extract.x, random_battery.x]
+        y = [house_to_extract.y, random_battery.y, random_battery.y]
+        routes[random_battery].append((x, y))
+        random_battery.add_house(house_to_extract)
+
+        # add random house to desired battery
+        desired_battery.add_house(random_house)
+        x = [random_house.x, random_house.x, desired_battery.x]
+        y = [random_house.y, desired_battery.y, desired_battery.y]
+        routes[desired_battery].append((x,y))
+
+    return routes
+
+def hill_climbing(batteries, routes, optimization_type, iterations, random_swap_every_x, plot_grid):
+    
+    plot_fig = False
+    colors = ['r', 'b', 'g', 'c', 'y']
+    all_cable_distance = []
+
+    # perform all swaps and save cable distances/costs
+    for iteration in range(iterations):
+        
+        # set plot_fig to true when is last iteration
+        if iteration == iterations-1 and plot_grid:
+            plot_fig = True
+
+        # create new routes with swap function, based on optimization type
+        if optimization_type == "stochastic_hill_climber" or optimization_type == "steepest_ascent_hill_climber":
+            routes = swap(routes, batteries, iteration, random_swap_every_x, optimization_type)
+        elif optimization_type == "simulated_annealing":
+            # TODO: call function
+            pass
+
+        # plot batteries and houses
+        if plot_fig:
+            plt.figure()
+
+            for battery in batteries:
+                plt.plot(battery.x, battery.y, colors[battery.id-1]+'o', markersize=10, label='batteries')
+                for house in battery.houses:
+                    plt.plot(house.x, house.y, 'k*')
+            plt.grid()
+            plt.legend(loc='upper center', ncol=10, fontsize=8)
+
+        cable_distance = 0
+        
+        # calculate costs and plot lines
         for battery in batteries:
             for coordinates_x, coordinates_y in routes[battery]:
-                plt.plot(coordinates_x, coordinates_y, colors[battery.id -1])
+
+                if plot_fig:
+                    plt.plot(coordinates_x, coordinates_y, colors[battery.id -1])
+                
+                # calculate distance
                 cable_distance += abs(coordinates_x[0]-coordinates_x[2]) + abs(coordinates_y[0] - coordinates_y[2])
         
-        print(cable_distance)
-        print(cable_distance*9)
+        if plot_fig:
+            plt.title(f"cable costs:{cable_distance * 9}")
+            plt.savefig(f"grid_{optimization_type}")
+            plt.close()
+        
+        # append cable distance for these options
+        all_cable_distance.append(cable_distance)
 
+    return all_cable_distance
+
+def optimize(batteries, routes, optimization_type, iterations, random_swap_every_x, plot_costs, plot_grid):
+
+    if optimization_type == "none":
+        iterations = 1
+
+    all_costs = hill_climbing(batteries, routes, optimization_type, iterations, random_swap_every_x, plot_grid)
+    print(min(all_costs))
+
+    if plot_costs:
+        plt.figure()
+        plt.title(f"min_costs:{min(all_costs) * 9}")
+        plt.xlabel("iteration")
+        plt.ylabel("costs (distance * 9)")
+        plt.plot(all_costs)
         plt.show()
+        plt.savefig(f"{optimization_type}")
+
--- smartgrid.py
+++ smartgrid.py
@@ -5,24 +5,66 @@ from route import Route
 
 # Functions
 from import_csv import import_district, import_batteries
-from grid import create_grid
+from grid import optimize, hill_climbing, swap
 
 
 def main():
-    # load houses from district1 and get house coordinates
-    district, x_houses, y_houses = import_district(1)
-    # print(district)
 
-    # load batteries from district1 and get battery coordinates
-    batteries = import_batteries(1)
-    # print(batteries)
+    # possible district, algorithm and optimization options 
+    district_options = [1, 2, 3]
+    algorithm_types = ["reverse", "non_reverse", "random"]
+    optimization_types = ["none", "stochastic_ascent_hill_climber", "steepest_ascent_hill_climber", "simulated_annealing"]
 
-    # calculates and stores the routes
-    r = Route()
-    routes = r.import_routes(district, batteries)
-    # plots the district and batteries in a grid
-    create_grid(district, x_houses, y_houses, batteries, routes)
+    # choose which option to use
+    district_nr = district_options[0] 
+    algorithm_type = algorithm_types[0]
+    optimization_type = optimization_types[2]
+    extra_optimzation = "Prim"
 
+    # other variables needed (when optimization_type is not "none")
+    iterations = 1000  # = number of swaps
+    random_swap_every_x = 100
+
+    # change if want to plot
+    plot_costs = True
+    plot_grid = True
+    random_swaps = True
+
+    # when you don't want random swaps
+    if not random_swaps:
+        random_swap_every_x = iterations
+    
+    # parameters for simulated_annealing
+    if optimization_type == "simulated_annealing":
+        temperature = 90
+        cooling_factor = 0.03
+
+    # load houses from district and get house coordinates
+    district, x_houses, y_houses = import_district(district_nr)
+
+    # load batteries from district and get battery coordinates
+    batteries = import_batteries(district_nr)
+
+    # based on the algorithms and optimization methods above, run different functions
+    if algorithm_type == "random":
+        pass
+        # import random_routes
+        # TODO: create function for random route
+        r = Route()
+        routes = r.import_routes(district, batteries) 
+        optimize(batteries, routes, optimization_type, iterations, random_swap_every_x, plot_costs, plot_grid)    
+    
+    # when not random algorithm_type 
+    else:
+        r = Route()
+        routes = r.import_routes(district, batteries) # add algorithm_type, so will 
+        optimize(batteries, routes, optimization_type, iterations, random_swap_every_x, plot_costs, plot_grid)
+
+# TODO: create different route files (random + reverse and non_reverse)
+# TODO: adjust placement of last house in route file so it will search in different batteries when last house cannot be placed
+# TODO: add simulated_annealing combination
+# TODO: add sharing grids/cables (Prims)
+# TODO: save good options, can't go back to them right now (when you do bad random swap, it's gone)
 
 if __name__ == "__main__":
     main()
